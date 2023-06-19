7fe227d8d730e9c802e874f12176e8881b7050c9
julielaursen <julielaursen1@gmail.com>
2023-06-19 11:31:33 -0500

    finish pages project

---
+++ pages/blog/[...slug].js
@@ -0,0 +1,13 @@
+import { useRouter } from 'next/router'
+
+function BlogPostsPage() {
+  const router = useRouter()
+  console.log(router.query)
+  return (
+    <div>
+      <h1>Blog Post</h1>
+    </div>
+  )
+}
+
+export default BlogPostsPage
---
+++ pages/clients/[id]/index.js
@@ -0,0 +1,18 @@
+import { useRouter } from 'next/router'
+
+function ClientsProjectsPage() {
+  const router = useRouter()
+  console.log(router.query)
+
+  function loadProjectHandler() {
+    router.push('/clients/max/projecta')
+  }
+  return (
+    <div>
+      <h1>The Projects of a Given Client</h1>
+      <button onClick={loadProjectHandler}>Load Project A</button>
+    </div>
+  )
+}
+
+export default ClientsProjectsPage
